a53ada28e73b6028cfbaaad6ac4f5c09d977aced
payalmote <https://github.com/payalmote>
2025-09-03 23:36:05 +0530

    greet without age

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 greet = input("Enter the your name:")
 age = int(input("enter your age: "))
 print(f"Hello, {greet}!")
-print(f"Welcome to local repo, {greet}({age})!")
+print(f"Welcome to local repo, {greet}!")
